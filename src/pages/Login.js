7106f612688a6c9cbf3735053bef7bc41fc6cbc3
tharindu125 <nawanjanaenda@gmail.com>
2023-04-01 00:20:47 +0530

    proxy change

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -7,7 +7,7 @@ export default function Login() {
 
     const login = async()=> {
         try {
-            const res = await fetch('/login',{
+            const res = await fetch('https://pharmacy-register-cyt4.onrender.com/login',{
                 method:'POST',
                 headers:{
                     'Content-Type':'application/json'
